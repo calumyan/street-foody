e5ca208bbfdb1d6c50d82230be60ea8fbb9cb875
Carrie003 <carries003@gmail.com>
2021-02-03 15:07:17 -0600

    created a food class and a streetvendor class

---
+++ Models/Food.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+
+
+namespace street_foody.Models
+{
+    
+    public class Food
+    {
+
+        public string Name{get; set:}
+
+        public List<string> Categories{get;set;}
+
+        public double Rating{get; set;}
+
+        [JSInvokable]
+        public string GetName(){
+
+            return FirstName + " " + LastName;
+        }
+
+        [JSInvokable]
+        public string GetCategories(){
+
+            return Categories;
+        }
+
+
+        
+
+
+        // public List<String> FoodList;
+
+
+
+
+    }
+}
