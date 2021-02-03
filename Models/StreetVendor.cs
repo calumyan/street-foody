9b13bdf291a818b6fff2ece2e9bda4ef8b3aee79
Carrie003 <carries003@gmail.com>
2021-02-03 13:41:24 -0600

    new commit

---
+++ Models/StreetVendor.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace street_foody.Models
+{
+    public class StreetVendorModel
+    {
+        public string Name{ get; set; }
+
+        public string Location{get;set;}
+
+        public List<String> FoodList;
+
+
+    }
+}
