57255b3147cb5df52efd8e5b40819ce024575475
Carrie003 <carries003@gmail.com>
2021-02-26 14:55:03 -0600

    changed name

--- Models/Food.cs
+++ Models/Food.cs
@@ -12,9 +12,9 @@ namespace street_foody.Models
     {
         [Key]
         public string FoodID{get;set;}
-        public string VietameseName{get; set;}
+        public string FoodVietnameseName{get; set;}
+        public string FoodEnglishName{get;set;}
         public List<string> Categories{get;set;}      
-        public string EnglishName{get;set;}
         public string Description{get;set;}
         public double Price{get;set;}
         public List<FoodCategory> FoodCategories{get;set;}  
