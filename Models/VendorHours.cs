9db7cbd14ee0694fc84f8d25f335e1b804e78596
mklapkowski <mklapkow@macalester.edu>
2021-03-09 01:37:05 -0500

    vendorhours update

--- Models/VendorHours.cs
+++ Models/VendorHours.cs
@@ -11,6 +11,7 @@ namespace street_foody.Models
         [ForeignKey("StreetVendor")]
         public string VendorID{get; set;}
         [Key]
+<<<<<<< Updated upstream
         public string VendorHoursID{get; set;}
         public string Location{get;set;}
         public string VendorDates{get;set;}
@@ -18,5 +19,19 @@ namespace street_foody.Models
         public string EndTime {get;set;}
 
         
+=======
+        public string VendorHoursInfo{
+        get{return VendorHoursInfo;} 
+        set{VendorHoursInfo = VendorID + " " +Location;}}
+        public string Location{get;set;}
+        public DateTime[] Sun {get;set;}
+        public DateTime[] Mon {get;set;}
+        public DateTime[] Tue {get;set;}
+        public DateTime[] Wed {get;set;}
+        public DateTime[] Thu {get;set;}
+        public DateTime[] Fri {get;set;}
+        public DateTime[] Sat {get;set;}
+        // select * from vendor where id in (select vendor_id from vendor_hours where start_time < ? and end_time > ?);
+>>>>>>> Stashed changes
     }
 }
