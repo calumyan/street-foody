e27efc4b8f75f5cff60ff0d60f21e2b5406126ea
Lucy Tran <ltran2@macalester.edu>
2021-03-10 20:40:20 -0600

    Add filter col animation and responsiveness for search page. Please accept incoming changes related to this

--- Models/VendorHours.cs
+++ Models/VendorHours.cs
@@ -10,22 +10,17 @@ namespace street_foody.Models
     }
     public class VendorHours
     {
-<<<<<<< HEAD
-    
+        [ForeignKey("StreetVendor")]
+        public string VendorID;
+
         [Key]
         public string VendorHoursID{get; set;}
-        public StreetVendor Vendor{get; set;}
-=======
-
-        [ForeignKey("StreetVendor")]
-        [Key]
-        public string VendorID{get; set;}
         // public string VendorHoursInfo{
         // get{return VendorHoursInfo;} 
         // set{VendorHoursInfo = VendorID + " " +Location;}}
 
+
         [Required]
->>>>>>> 8e6e8479efaa0f78436647e6d8fa3012330ad543
         public string Location{get;set;}
 
         // Each day column is not required. To specify a closed day, leave the column null.
